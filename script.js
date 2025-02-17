a7ad87140bc1e31c585d0786a315925062b0d0be
Clarance Williams <125715028+Quindarywheatherspoon@users.noreply.github.com>
2025-02-17 15:26:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const button10Minutes = document.getElementById("button-10-minutes");
+  document
+    .getElementById("button-10-minutes")
+    .addEventListener("click", function () {
+      const newTab = window.open("about:blank", "_blank");
+      newTab.document.body.style.margin = "0";
+      newTab.document.body.style.padding = "0";
 
-  button10Minutes.addEventListener("click", function () {
-    // Open a new tab
-    const newTab = window.open("about:blank", "_blank");
-    newTab.document.body.style.margin = "0";
-    newTab.document.body.style.padding = "0";
+      const objectTag = document.createElement("object");
+      objectTag.data =
+        "https://abnormal-hosting.netlify.app/10-minutes-till-dawn/index.html";
+      objectTag.type = "text/html";
+      objectTag.style.width = "100%";
+      objectTag.style.height = "100vh";
 
-    const objectTag = document.createElement("object");
-    objectTag.data =
-      "https://abnormal-hosting.netlify.app/10-minutes-till-dawn/index.html";
-    objectTag.type = "text/html";
-    objectTag.style.width = "100%";
-    objectTag.style.height = "100vh";
-
-    newTab.document.body.appendChild(objectTag);
-  });
+      newTab.document.body.appendChild(objectTag);
+    });
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  const button10Minutes = document.getElementById("button-2048");
-
-  button10Minutes.addEventListener("click", function () {
-    // Open a new tab
+  document.getElementById("button-2048").addEventListener("click", function () {
     const newTab = window.open("about:blank", "_blank");
     newTab.document.body.style.margin = "0";
     newTab.document.body.style.padding = "0";
@@ -35,4 +31,42 @@ document.addEventListener("DOMContentLoaded", function () {
 
     newTab.document.body.appendChild(objectTag);
   });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("button-bitlife")
+    .addEventListener("click", function () {
+      const newTab = window.open("about:blank", "_blank");
+      newTab.document.body.style.margin = "0";
+      newTab.document.body.style.padding = "0";
+
+      const objectTag = document.createElement("object");
+      objectTag.data =
+        "https://abnormal-hosting.netlify.app/bitlife/index.html";
+      objectTag.type = "text/html";
+      objectTag.style.width = "100%";
+      objectTag.style.height = "100vh";
+
+      newTab.document.body.appendChild(objectTag);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("button-binding")
+    .addEventListener("click", function () {
+      const newTab = window.open("about:blank", "_blank");
+      newTab.document.body.style.margin = "0";
+      newTab.document.body.style.padding = "0";
+
+      const objectTag = document.createElement("object");
+      objectTag.data =
+        "https://isaac-but-abnormal.netlify.app/";
+      objectTag.type = "text/html";
+      objectTag.style.width = "100%";
+      objectTag.style.height = "100vh";
+
+      newTab.document.body.appendChild(objectTag);
+    });
 });
