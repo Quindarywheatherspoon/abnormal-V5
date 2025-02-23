82742a50309716f7fbf52a24ffa9403b1f30c5fc
Clarance Williams <125715028+Quindarywheatherspoon@users.noreply.github.com>
2025-02-22 21:05:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ document.addEventListener("DOMContentLoaded", () => {
         newTab.document.write(`
           <html>
             <head>
-              <title>Bell Schedule</title>
+              <title>New Tab</title>
               <style>
                 body, html { margin: 0; padding: 0; height: 100%; overflow: hidden; }
                 object { width: 100%; height: 100vh; border: none; }
